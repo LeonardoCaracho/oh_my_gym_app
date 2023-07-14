7aae31b58b91a6379a45a044ac84359777778ad5
Leonardo Caracho <leeo.apc@gmail.com>
2023-07-13 21:05:45 -0300

    feat: create package

---
+++ packages/workout_repository/lib/src/workout_repository.dart
@@ -0,0 +1,7 @@
+/// {@template workout_repository}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class WorkoutRepository {
+  /// {@macro workout_repository}
+  const WorkoutRepository();
+}
---
+++ packages/workout_repository/lib/workout_repository.dart
@@ -0,0 +1,4 @@
+/// A Very Good Project created by Very Good CLI.
+library workout_repository;
+
+export 'src/workout_repository.dart';
