488b1dcc335f5794c00cd2c6e7d47f18ffca1cf2
Leonardo Caracho <leeo.apc@gmail.com>
2023-09-06 06:51:11 -0300

    feat: change history repository

--- packages/history_repository/lib/src/contracts/history_contract.dart
+++ packages/history_repository/lib/src/contracts/history_contract.dart
@@ -1,7 +1,7 @@
 import 'package:workouts_api/workouts_api.dart';
 
 abstract class HistoryContract {
-  Future<void> saveRecord(WorkoutHistory workout);
+  Future<void> saveRecord(WorkoutHistory record);
   Future<List<WorkoutHistory>> getRecords();
   Future<void> deleteRecord(String docId);
 }
--- packages/history_repository/lib/src/impl/history_repository.dart
+++ packages/history_repository/lib/src/impl/history_repository.dart
@@ -2,18 +2,22 @@ import 'package:history_repository/history_repository.dart';
 import 'package:workouts_api/workouts_api.dart';
 
 class HistoryRepository extends HistoryContract {
+  HistoryRepository(this.historyApi);
+
+  final HistoryApi historyApi;
+
   @override
-  Future<void> deleteRecord(String docId) {
-    throw UnimplementedError();
+  Future<void> deleteRecord(String docId) async {
+    await historyApi.deleteRecord(docId);
   }
 
   @override
-  Future<List<WorkoutHistory>> getRecords() {
-    throw UnimplementedError();
+  Future<List<WorkoutHistory>> getRecords() async {
+    return historyApi.getRecords();
   }
 
   @override
-  Future<void> saveRecord(WorkoutHistory workout) {
-    throw UnimplementedError();
+  Future<void> saveRecord(WorkoutHistory record) async {
+    await historyApi.saveRecord(record);
   }
 }
