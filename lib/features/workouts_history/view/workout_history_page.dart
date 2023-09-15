aa6e06aa48b7d3a8676a495e7e73e8639a823ede
Leonardo Caracho <leeo.apc@gmail.com>
2023-09-14 21:04:26 -0300

    fix: analyze

--- lib/features/workouts_history/view/workout_history_page.dart
+++ lib/features/workouts_history/view/workout_history_page.dart
@@ -1,4 +1,3 @@
-import 'package:app_ui/app_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:oh_my_gym_app/core/core.dart';
 import 'package:oh_my_gym_app/features/workouts_history/workouts_history.dart';
