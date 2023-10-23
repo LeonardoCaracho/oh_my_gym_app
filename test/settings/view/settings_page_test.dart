afd8966854dca61c0399abbd8942504f3d3890f8
Leonardo Caracho <leeo.apc@gmail.com>
2023-10-23 18:43:58 -0300

    fix: tests

--- test/settings/view/settings_page_test.dart
+++ test/settings/view/settings_page_test.dart
@@ -1,19 +1,11 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
-import 'package:oh_my_gym_app/features/settings/settings.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:oh_my_gym_app/features/settings/settings.dart';
 
 void main() {
   group('SettingsPage', () {
-    group('route', () {
-      test('is routable', () {
-        expect(SettingsPage.route(), isA<MaterialPageRoute>());
-      });
-    });
-
     testWidgets('renders SettingsView', (tester) async {
-      await tester.pumpWidget(MaterialApp(home: SettingsPage()));
+      await tester.pumpWidget(const MaterialApp(home: SettingsPage()));
       expect(find.byType(SettingsView), findsOneWidget);
     });
   });
--- test/settings/view/widgets/settings_body_test.dart
+++ test/settings/view/widgets/settings_body_test.dart
@@ -1,16 +1,14 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
-import 'package:oh_my_gym_app/features/settings/settings.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:oh_my_gym_app/features/settings/settings.dart';
 
 void main() {
   group('SettingsBody', () {
-    testWidgets('renders Text', (tester) async { 
+    testWidgets('renders Text', (tester) async {
       await tester.pumpWidget(
         BlocProvider(
           create: (context) => SettingsCubit(),
-          child: MaterialApp(home: SettingsBody()),
+          child: const MaterialApp(home: SettingsBody()),
         ),
       );
 
